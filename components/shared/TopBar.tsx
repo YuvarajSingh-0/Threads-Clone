1c9fe872ecd767886fd4a1da0bffa4e117045b32
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2024-02-23 19:07:12 +0530

    login button

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
-import {dark} from '@clerk/themes';
+import { SignedIn, SignOutButton, OrganizationSwitcher, currentUser } from "@clerk/nextjs";
+import { dark } from '@clerk/themes';
 
-export default function TopBar() {
+export default async function TopBar() {
+    const user = await currentUser();
     return (
         <nav className="fixed top-0 z-30 flex w-full items-center justify-between bg-dark-2 px-6 py-3">
             <Link className="flex items-center gap-4" href="/">
@@ -20,14 +21,20 @@ export default function TopBar() {
                         </SignOutButton>
                     </SignedIn>
                 </div>
-                <OrganizationSwitcher
-                    appearance={{
-                        baseTheme:dark,
-                        elements: {
-                            organizationSwitcherTrigger: "py-2 px-4"
-                        }
-                    }}
-                />
+                {user ?
+                    <OrganizationSwitcher
+                        appearance={{
+                            baseTheme: dark,
+                            elements: {
+                                organizationSwitcherTrigger: "py-2 px-4"
+                            }
+                        }}
+                    /> : 
+                    <Link href={'/sign-in'} className="flex cursor-pointer gap-4 p-4">
+                        <Image src="/assets/logout.svg" width={24} height={24} alt="logout" />
+                        <p className="text-light-2 max-lg:hidden">Login</p>
+                    </Link>
+                    }
             </div>
         </nav>
     )
